6e6fdb8ae1d4fca5702f5c9c0225729ebd47cd42
YoutechA320U <42425276+YoutechA320U@users.noreply.github.com>
2019-03-13 18:53:24 +0900

    Update ttymidi.c

--- ttymidi.c
+++ ttymidi.c
@@ -423,10 +423,10 @@ void write_midi_to_alsa(snd_seq_t* seq, int port_out_id, char *buf, int buflen)
 
 		case 0xE0:
 			//param1 = (param1 & 0x7F) + ((param2 & 0x7F) << 7);
+			param1 = (param1 & 0x7F) + ((param2 & 0x7F) << 7);
 			if (!arguments.silent && arguments.verbose)
-				printf("Serial  0x%02X Pitch Bend         %03u %03u %03u\n", operation, channel, param1, param2);
-			snd_seq_ev_set_pitchbend(&ev, channel, param1, param2);
-			//snd_seq_ev_set_pitchbend(&ev, channel, param1); // in alsa MIDI we want signed int
+				printf("Serial  0x%02X Pitch bend         %03u %05i\n", operation, channel, param1);
+			snd_seq_ev_set_pitchbend(&ev, channel, param1 - 8192); // in alsa MIDI we want signed int
 			break;
 
 		case 0xF0:

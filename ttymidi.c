512c5fb6bcc9e428dcc7d7c01b25bcf3e25a9105
YoutechA320U <sax_buraban_521@yahoo.co.jp>
2020-01-29 00:33:41 +0900

    Update ttymidi.c

--- ttymidi.c
+++ ttymidi.c
@@ -261,8 +261,8 @@ void write_midi_action_to_serial_port(snd_seq_t* seq_handle)
 			case SND_SEQ_EVENT_PITCHBEND:
 				bytes[0] = 0xE0 + ev->data.control.channel;
 				ev->data.control.value += 8192;
-				bytes[1] = ev->data.control.value & 0x7;
-				bytes[2] = ev->data.control.value >> 7;
+				bytes[1] = (int)ev->data.control.value & 0x7F;
+				bytes[2] = (int)ev->data.control.value >> 7;
 				if (!arguments.silent && arguments.verbose)
 					printf("Alsa    0x%02X Pitch bend         %03u %03u %03u\n", bytes[0]&0xF0, bytes[0]&0xF, bytes[1], bytes[2]);
 				break;
